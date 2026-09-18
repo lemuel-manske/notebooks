0ff0064432eb6b7fc8ebc0ef63b4b088bbef7c62
lemuel-manske <lemuelkaue@gmail.com>
2026-09-18 10:16:44 -0300

    feat(furb/ml): continue working on `trabalho_2.py`

--- furb/ml/trabalho_2.py
+++ furb/ml/trabalho_2.py
@@ -1,4 +1,3 @@
-import statistics
 import math
 
 import scipy.io as scipy
@@ -10,18 +9,20 @@ DATASET_3 = scipy.loadmat('trabalho_2_parte_1_3.mat')
 DATASET_4 = scipy.loadmat('trabalho_2_parte_1_4.mat')
 
 
-type Vector = list[int]
-
-def d(x: Vector, y: Vector) -> float:
+def d(x, y) -> float:
     return math.sqrt(sum((xi - yi) ** 2 for xi, yi in zip(x, y)))
 
 
-def knn(
-    train: list[list[int]],  # 100 x 4
-    labels: list[str],  # 100 x 1
-    test: list[list[int]],  # 100 x 4
+def mode(lst):
+    return max(lst, key=lambda x: (lst.count(x), -lst.index(x)))
+
+
+def kNN(
+    train,
+    labels,
+    test,
     k: int,
-) -> list[str]:
+) -> list:
     pred = []
     for t in test:
         dists = []
@@ -29,23 +30,81 @@ def knn(
             dists.append((ti, d(train[ti], t)))
         dists.sort(key=lambda d_: d_[1])
         nn = dists[:k]
-        pred.append(labels[nn[0][0]])  # Política de decisão: sempre retornar o primeiro
-    return max(set(pred), key=pred.count)
+        nn_labels = [labels[i][0] for i, _ in nn]  # política de decisão: sempre retornar o primeiro
+        pred.append(mode(nn_labels))
+    return pred
 
 
-def demo_knn(dataset, k):
-    res = knn(
+def acc(nn, labels):
+    labels = [
+        label[0]
+        for label in labels
+    ]  # flatten
+
+    return sum(r == e for r, e in zip(nn, labels)) / len(labels)
+
+
+def find_best_k(
+    train,
+    train_labels,
+    test,
+    test_labels,
+) -> int:
+    best_acc = 0.0
+    best_k = 0
+    for i in range(1, len(train)):  # testar todos os `k` possíveis
+        curr_acc = acc(kNN(train, train_labels, test, i), test_labels)
+        if curr_acc > best_acc:
+            best_acc = curr_acc
+            best_k = i
+    return best_k
+
+
+def demo_knn(dataset):
+    show_chart(dataset['grupoTrain'], dataset['trainRots'], 1, 2)
+
+    print()
+
+    best_k = find_best_k(
         dataset['grupoTrain'],
         dataset['trainRots'],
         dataset['grupoTest'],
-        k,
+        dataset['testRots'],
     )
 
     print(
-        sum(res == DATASET_1['testRots']) / len(DATASET_1['testRots'])
+        f"Best k: {best_k}"
+    )
+
+    res = kNN(
+        dataset['grupoTrain'],
+        dataset['trainRots'],
+        dataset['grupoTest'],
+        best_k,
+    )
+
+    print(
+        f"Acc: {acc(res, dataset['testRots'])}"
     )
 
 
+def get_label(data, labels, label, idx):
+    ret = []
+    for i in range(len(data)):
+        if(labels[i] == label):
+            ret.append(data[i][idx])
+    return ret
+
+
+def show_chart(data, labels, d1, d2):
+    import matplotlib.pyplot as plt
+
+    fig, ax = plt.subplots()
+    ax.scatter(get_label(data, labels, 1, d1), get_label(data, labels, 1, d2), c='red' , marker='^')
+    ax.scatter(get_label(data, labels, 2, d1), get_label(data, labels, 2, d2), c='blue' , marker='+')
+    ax.scatter(get_label(data, labels, 3, d1), get_label(data, labels, 3, d2), c='green', marker='.')
+    plt.show()
+
+
 if __name__ == "__main__":
-    demo_knn(DATASET_1, k=1)
-    demo_knn(DATASET_1, k=10)
+    demo_knn(DATASET_1)
